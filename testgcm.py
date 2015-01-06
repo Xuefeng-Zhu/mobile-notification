3ef0aabcdb22b58320b015b553ce095d6da847bb
Xuefeng Zhu <xzhu15@illinois.edu>
2015-01-05 20:56:57 -0600

    Update testgcm.py

--- testgcm.py
+++ testgcm.py
@@ -2,5 +2,5 @@ from gcm import GCM
 gcm = GCM('AIzaSyAJ882Hvz_hEacehne7TOIYn-uUvT0LI9s')
 reg_id = 'APA91bG9CDDMm7njzdvmoKpLaQI_Y9_UnCf6E1nmh28YrVz6-xSD4wOOHBgM7SUrjp-Rundmanm9j4Ke9oz0LvQGhrH4OS1cK0QhNDtSJK4VRUt9o6Tcm3qKd3miP8NKbzFqH8E_aqtefzve8W-Zs7rElg5vay2vEx29tRAMChOmAWvBBncmOew'
 
-data = {'message': 'data'}
+data = {'message': 'hello world'}
 gcm.plaintext_request(registration_id=reg_id, data=data)
